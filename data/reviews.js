b547a5acc520a583dc00ff5b0848595ae9346895
Mihir Joshi <mihirj961@gmail.com>
2021-11-18 16:13:46 -0500

    fixed review

--- data/reviews.js
+++ data/reviews.js
@@ -1,31 +1,28 @@
 const mongoCollections = require("../config/mongoCollections");
 const reviews = mongoCollections.reviews;
 const { ObjectId } = require("mongodb");
-const create = async function create(recipeID, userID, rating, review) {
+const create = async function create(recipeID, userID, rating) {
   //error handling
-  if (arguments.length !== 4) throw "Invalid Arguments";
+  if (arguments.length !== 3) throw "Invalid Arguments";
   if (
     recipeID == undefined ||
     userID == undefined ||
-    rating == undefined ||
-    review == undefined
+    rating == undefined
   )
     throw "Enter All parameters review";
   if (
     typeof recipeID !== "string" ||
     typeof userID !== "string" ||
-    typeof review !== "string" ||
     typeof rating !== "number"
   )
     throw "Invalid type";
-  if (recipeID.length === 0 || userID.length === 0 || review.length === 0)
+  if (recipeID.length === 0 || userID.length === 0)
     throw "String empty";
   if (!recipeID.trim() || !userID.trim() || !review.trim())
     throw "String contains only spaces";
 
   recipeID = recipeID.trim();
   userID = userID.trim();
-  review = review.trim();
   if (rating < 0) throw "Overall rating invalid";
   if (rating > 5) throw "Overall rating invalid";
   if (!ObjectId.isValid(recipeID)) throw "Invalid Recipe ID";
@@ -35,7 +32,6 @@ const create = async function create(recipeID, userID, rating, review) {
     recipeID: recipeID,
     userID: userID,
     rating: rating,
-    review: review,
   };
   const insertInfo = await reviewCollection.insertOne(revObj);
   if (insertInfo.insertedCount === 0) throw "Could not add restaurant";
