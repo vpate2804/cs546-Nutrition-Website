44af7ec9f9fd4daefa2abb9b5900a195bb8b788e
Mihir Joshi <mihirj961@gmail.com>
2021-11-29 17:02:58 -0500

    merge

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 const userRoutes = require('./log');
+const allRoutes = require("./allrecipes");
 const path = require('path');
 
 const constructorMethod = (app) => {
     app.use('/', userRoutes);
-    
+    app.use('/allrecipes', allRoutes);
   
     app.use('*', (req, res) => {
       res.status(404).json({error: 'Not found'});
