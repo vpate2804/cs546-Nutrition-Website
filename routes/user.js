03f2e485e769377141d47390e4b83405971c5190
Mihir Joshi <mihirj961@gmail.com>
2021-12-04 13:49:57 -0500

    fix

--- routes/user.js
+++ routes/user.js
@@ -4,7 +4,7 @@ const data = require("../data");
 const ObjectId = require("mongodb").ObjectId;
 const userData = data.users;
 const recipesData = data.recipes;
-const xss = require('xss');
+const xss = require("xss");
 
 router.get("/private", async (req, res) => {
   if (req.session.user) {
@@ -27,32 +27,33 @@ router.get("/private", async (req, res) => {
         id: favoriteRecipesId[i],
       };
     }
-}})
-router.post('/private', async (req, res) => {
-    if (req.session.user) {
-        let username = req.session.user;
-        let userInfo = await userData.getUserByUsername(username);
-        let firstName = xss(req.body.firstname);
-        let lastName = xss(req.body.lastname);
-        let email = xss(req.body.email);
-        let userId = userInfo._id.toString();
-        let deleteFavoritesRecipesId = req.body.favoriteRecipesNameDeleteID;
-        let updateInfo = {
-            firstname: firstName,
-            lastname: lastName,
-            email: email
-        }
-        try {
-            let updateResult = await userData.updateUser(userId, updateInfo);
-        } catch (e) {
-            
-        }
-        try {
-            for (let i = 0; i < deleteFavoritesRecipesId.length; i++) {
-                let deleteFavoritesRecipes = await userData.deleteToFavorite(userId, deleteFavoritesRecipesId[i]);
-            }
-        } catch (e) {
-
+  }
+});
+router.post("/private", async (req, res) => {
+  if (req.session.user) {
+    let username = req.session.user;
+    let userInfo = await userData.getUserByUsername(username);
+    let firstName = xss(req.body.firstname);
+    let lastName = xss(req.body.lastname);
+    let email = xss(req.body.email);
+    let userId = userInfo._id.toString();
+    let deleteFavoritesRecipesId = req.body.favoriteRecipesNameDeleteID;
+    let updateInfo = {
+      firstname: firstName,
+      lastname: lastName,
+      email: email,
+    };
+    try {
+      let updateResult = await userData.updateUser(userId, updateInfo);
+    } catch (e) {}
+    try {
+      for (let i = 0; i < deleteFavoritesRecipesId.length; i++) {
+        let deleteFavoritesRecipes = await userData.deleteToFavorite(
+          userId,
+          deleteFavoritesRecipesId[i]
+        );
+      }
+    } catch (e) {}
     res.render("private", {
       userName: username,
       firstName: firstName,
@@ -63,6 +64,7 @@ router.post('/private', async (req, res) => {
       islogin: islogin,
     });
   }
+});
 router.post("/private", async (req, res) => {
   if (req.session.user) {
     let username = req.session.user;
@@ -116,63 +118,73 @@ router.post("/private", async (req, res) => {
   }
 });
 
-router.get('/addNewRecipe', async (req, res) => {
-    if (req.session.user) {
-        let title = "Signup";
-        let islogin = true;
-        res.render('addNewRecipe', { title: title, islogin: islogin });
-        // return;
-    }
-})
+router.get("/addNewRecipe", async (req, res) => {
+  if (req.session.user) {
+    let title = "Signup";
+    let islogin = true;
+    res.render("addNewRecipe", { title: title, islogin: islogin });
+    // return;
+  }
+});
 
-router.post('/addNewRecipe', async (req, res) => {
-    if (req.session.user) {
-        console.log(req.body);
-        let name = xss(req.body.name);
-        let ingredients = xss(req.body.ingredients);
-        let preparationTime = xss(parseInt(req.body.preparationTime));
-        let cookTime = xss(parseInt(req.body.cookTime));
-        let recipeType = xss(req.body.recipeType);
-        let foodGroup = xss(req.body.foodGroup);
-        let season = xss(req.body.season);
-        let nutritionDetails = xss(req.body.nutritionDetails);
-        let recipeSteps = xss(req.body.recipeSteps);
-        try {
-            let createRecipe = await recipesData.createRecipe(name, ingredients,preparationTime,cookTime,recipeType,foodGroup, season,nutritionDetails,recipeSteps)
-            let islogin = true;
-            let title = "Private";
-            console.log(createRecipe)
-            res.render('private', {
-                title: title,
-                islogin: islogin
-            });
-        }catch (e) {
-            console.log(e)
-        }
+router.post("/addNewRecipe", async (req, res) => {
+  if (req.session.user) {
+    console.log(req.body);
+    let name = xss(req.body.name);
+    let ingredients = xss(req.body.ingredients);
+    let preparationTime = xss(parseInt(req.body.preparationTime));
+    let cookTime = xss(parseInt(req.body.cookTime));
+    let recipeType = xss(req.body.recipeType);
+    let foodGroup = xss(req.body.foodGroup);
+    let season = xss(req.body.season);
+    let nutritionDetails = xss(req.body.nutritionDetails);
+    let recipeSteps = xss(req.body.recipeSteps);
+    try {
+      let createRecipe = await recipesData.createRecipe(
+        name,
+        ingredients,
+        preparationTime,
+        cookTime,
+        recipeType,
+        foodGroup,
+        season,
+        nutritionDetails,
+        recipeSteps
+      );
+      let islogin = true;
+      let title = "Private";
+      console.log(createRecipe);
+      res.render("private", {
+        title: title,
+        islogin: islogin,
+      });
+    } catch (e) {
+      console.log(e);
     }
-})
+  }
+});
 router.post("/addfavorite", async (req, res) => {
-    if (req.session.user) {
-      console.log(req.session);
-      let username=req.session.user;
-      let userInfo=await userData.getUserByUsername(username);
-      let userID=userInfo._id.toString();
-      console.log(userID);
-      let favid = req.body.recipeId;
-      console.log(favid);
-      try {
-        let addFavorite = await userData.addToFavorite(userID, favid);
-        let recipeList = await recipesData.getAllRecipes();
-        if (addFavorite) {
-          res.redirect("/all")
-        }
-      } catch (e) {
-        console.log(e);
+  if (req.session.user) {
+    console.log(req.session);
+    let username = req.session.user;
+    let userInfo = await userData.getUserByUsername(username);
+    let userID = userInfo._id.toString();
+    console.log(userID);
+    let favid = req.body.recipeId;
+    console.log(favid);
+    try {
+      let addFavorite = await userData.addToFavorite(userID, favid);
+      let recipeList = await recipesData.getAllRecipes();
+      if (addFavorite) {
+        res.redirect("/all");
       }
-    } else {
-      //let recipeList = await recipesData.getAllRecipes();
-      res.redirect("/login");
+    } catch (e) {
+      console.log(e);
     }
-  });
+  } else {
+    //let recipeList = await recipesData.getAllRecipes();
+    res.redirect("/login");
+  }
+});
 
 module.exports = router;
