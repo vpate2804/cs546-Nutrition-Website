27cf4729db52fbade8d129bef1bbd8452cefe1c8
Jkang1221 <jiakangphil@163.com>
2021-12-05 22:48:00 -0500

    change app.js

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const express = require('express');
 const app = express();
 const static = express.static(__dirname + '/public');
 
-
 const configRoutes = require('./routes');
 const exphbs = require('express-handlebars');
 
@@ -16,29 +15,45 @@ app.set('view engine', 'handlebars');
 const session = require('express-session')
 
 app.use(session({
-  name: 'AuthCookie',   
+  name: 'AuthCookie',
   secret: 'some secret string!',
   resave: false,
   saveUninitialized: true
 }));
-app.use('/user/private', (req,res,next) => {
-  if(!req.session.user){
+
+
+
+app.use('/private', (req, res, next) => {
+  if (!req.session.user) {
     let title = "Error";
     let message = "You are not log in";
     res.status(403);
-    res.render('error',{title:title,error:message});
-  }else{
+    res.render('error', { title: title, error: message });
+  } else {
     next();
   }
 });
 
+// app.use(async(req,res,next) => {
+//   let time = new Date().toUTCString();
+//   let method = req.method;
+//   let route = req.originalUrl;
+//   if(req.session.user){
+//     let message = "Authenticated User";
+//     console.log(time+' '+method+' '+route+' '+message);
+//   }else{
+//     let message = "Non-Authenticated User";
+//     console.log(time+' '+method+' '+route+' '+message);
+//   }
+//   next();
+// });
+
 configRoutes(app);
 
-
 app.listen(3000, () => {
-    console.log("We've now got a server!");
-    console.log('Your routes will be running on http://localhost:3000');
-  });
+  console.log("We've now got a server!");
+  console.log('Your routes will be running on http://localhost:3000');
+});
 
 // const comments = require('./data/comments');
 // const recipes = require('./data/recipes');
