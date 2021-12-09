d6d3e6491bf6bde6be131799428cc4f16cee9ab7
vpate2804 <92997920+vpate2804@users.noreply.github.com>
2021-12-09 16:51:12 -0500

    fix

--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,14 @@ const userRoutes = require('./log');
 const userInfo = require('./user')
 const allRoutes = require("./allrecipes");
 const commentRoutes=require('./comment');
+const ratingRoutes=require('./rating');
 
 const constructorMethod = (app) => {
     app.use('/', userRoutes);
     app.use('/user', userInfo);
     app.use('/all', allRoutes);
     app.use('/comment',commentRoutes);
+    app.use('/rating',ratingRoutes);
   
     app.use('*', (req, res) => {
       res.status(404).render('404'); 
